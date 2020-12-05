4fe7d87025897697a54f89819cda32d95b1a41be
Jake O'Toole <otoolej33@gmail.com>
2020-12-05 11:37:12 -0600

    Correct incorrect direction in prompt. Said to look in far left, but it is the far right.

--- kill_localhost.bat
+++ kill_localhost.bat
@@ -1,5 +1,5 @@
 ECHO OFF
 SET /P PORT="What port is localhost on?: "
 netstat -ano | findstr %PORT%
-SET /P PID="What is the PID of the port (far left column)?: "
+SET /P PID="What is the PID of the port (far right column)?: "
 taskkill /PID %PID% -F 
